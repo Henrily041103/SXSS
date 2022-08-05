859b24d01b3be8e544a576441ce4a4b253dff157
chunhai2703 <trunghai27032003@gmail.com>
2022-08-05 08:23:39 +0700

    Update Homepage_Scripts

--- Frontend Scripts/Homepage_Scripts.js
+++ Frontend Scripts/Homepage_Scripts.js
@@ -2,9 +2,9 @@ var blog_content = document.getElementById("blog_window");
 var blog_mode = document.getElementById("blog");
 var info_content = document.getElementById("info_window");
 var info_mode = document.getElementById("info");
-var zoom_in_mode =document.getElementById("zoom_in");
-var zoom_out_mode = document.getElementById("zoom_out");
-var main_menu_content = document.getElementById("main_menu");
+var zoom_in=document.getElementById("zoom_in");
+var zoom_out= document.getElementById("zoom_out");
+var main_menu = document.getElementById("main_menu");
 var options = document.getElementById("options");
 function openBlog() {
     blog_content. style. display="flex";
@@ -15,10 +15,16 @@ function openInfo() {
     blog_content.style.display ="none";
 }
 function openZoomin(){
-
+    bigdiv1.style.display ="none";
+    bigdiv2.classname = "col-sm-11";
+    zoom_out_mode.style.display = "flex";
+    zoom_in_mode.style.display = "none";
 }
 function openZoomout(){
-
+    bigdiv1.style.display = "flex";
+    bigdiv2.classname = "col-sm-8";
+    zoom_out_mode.style.display = "none";
+    zoom_in_mode.style.display = "flex";
 }
 function options(){
 
