a466650eb079de847d389394ebec7517abf305de
Monk <anhmonkdv@gmail.com>
2022-08-07 16:46:39 +0700

    it's functional now

--- Frontend Scripts/Homepage_Scripts.js
+++ Frontend Scripts/Homepage_Scripts.js
@@ -1,33 +1,42 @@
-var blog_content = document.getElementById("blog_window");
-var blog_mode = document.getElementById("blog");
-var info_content = document.getElementById("info_window");
-var info_mode = document.getElementById("info");
-var zoom_in=document.getElementById("zoom_in");
-var zoom_out= document.getElementById("zoom_out");
-var main_menu = document.getElementById("main_menu");
-var options = document.getElementById("options");
-function openBlog() {
-    blog_content. style. display="flex";
-    info_content. style. display="none";
-}
-function openInfo() {
-    info_content.style.display ="flex";
-    blog_content.style.display ="none";
-}
-function openZoomin(){
-    bigdiv1.style.display ="none";
-    bigdiv2.classname = "col-sm-11";
-    zoom_out_mode.style.display = "flex";
-    zoom_in_mode.style.display = "none";
-}
-function openZoomout(){
-    bigdiv1.style.display = "flex";
-    bigdiv2.classname = "col-sm-8";
-    zoom_out_mode.style.display = "none";
-    zoom_in_mode.style.display = "flex";
-}
-function options(){
+document.addEventListener("DOMContentLoaded", () => {
+    var blog_content = document.getElementById("blog_window");
+    var blog_mode = document.getElementById("blog");
+    var info_content = document.getElementById("info_window");
+    var info_mode = document.getElementById("info");
+    var zoom_in=document.getElementById("zoom_in");
+    var zoom_out= document.getElementById("zoom_out");
+    var main_menu = document.getElementById("main_menu");
+    var options = document.getElementById("options");
+    var content = document.getElementById("content");
 
-}
-blog_mode.addEventListener("click", openBlog);
-info_mode.addEventListener("click", openInfo);
+    function openBlog() {
+        blog_content.style.display="flex";
+        info_content.style.display="none";
+    }
+    function openInfo() {
+        info_content.style.display ="flex";
+        blog_content.style.display ="none";
+    }
+    function openZoomin(){
+        main_menu.style.display ="none";
+        content.className = "col-sm-12";
+        zoom_out.style.display = "flex";
+        zoom_in.style.display = "none";
+    }
+    function openZoomout(){
+        main_menu.style.display = "flex";
+        content.className = "col-sm-9";
+        zoom_out.style.display = "none";
+        zoom_in.style.display = "flex";
+    }
+    function options(){
+
+    }
+    blog_mode.addEventListener("click", openBlog);
+    info_mode.addEventListener("click", openInfo);
+    zoom_in.addEventListener("click", openZoomin);
+    zoom_out.addEventListener("click", openZoomout);
+
+    blog_mode.click();
+    zoom_out.click();
+})
