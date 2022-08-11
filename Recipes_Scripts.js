0bcdc4e15af1f7e2d84a72577a5cd55f9af3c6ef
Monk <anhmonkdv@gmail.com>
2022-08-12 02:59:46 +0700

    major rework + react integration

---
+++ Recipes_Scripts.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./recipes.css";
+
+var element = React.createElement(
+    "h1",
+    null,
+    "Test here"
+);
+
+ReactDOM.render(element, document.getElementById("main_Stuff"));
